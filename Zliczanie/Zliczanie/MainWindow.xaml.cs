2d200c4eb85449ed690ed3fee3f3f37727287612
Lafantej18 <39798051+Lafantej18@users.noreply.github.com>
2020-07-04 18:18:15 +0200

    Add files via upload

---
+++ Zliczanie/Zliczanie/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Zliczanie
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ListBox.Items.Clear();
+
+            string napis;
+            char litera;
+
+            try
+            {
+                napis = TextBox_Napis.Text;
+                litera = char.Parse(TextBox_Litera.Text);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("W drugim polu tekstowym należy podać jeden znak! Zostanie pobrany pierwszy znak.");
+                napis = TextBox_Napis.Text;
+
+                if (TextBox_Litera.Text == "")
+                {
+                    litera = ' ';
+                }
+                else
+                {
+                    string tmp = TextBox_Litera.Text;
+                    litera = tmp[0];
+                }
+            }
+
+            Button_Click_Obliczanie(napis, litera);
+        }
+
+        private void Button_Click_Obliczanie(string napis, char litera)
+        {
+            int licznik = 0;
+
+            for (int i = 0; i < napis.Length; i++)
+            {
+                if (napis[i] == litera)
+                {
+                    licznik++;
+                    ListBox.Items.Add("[" + licznik + "]" + " Wystąpienie na pozycji: " + i);
+                }
+            }
+
+            if (licznik == 0)
+                ListBox.Items.Add("Znak nie wystąpił w napisie ani razu.");
+            else
+                ListBox.Items.Add("Ilość wystąpień znaku w danym napisie: " + licznik);
+        }
+    }
+}
